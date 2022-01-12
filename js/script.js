c1a072b991357592fe7f416c0d146025efa3b887
Adner16 <giovanni.renda16@gmail.com>
2022-01-12 14:51:36 +0100

    jsnacke 1

---
+++ js/script.js
@@ -0,0 +1,20 @@
+console.log('js ok');
+
+const numberOne = prompt('inserisci numero')
+const numberTwo = prompt('inserisci numero');;
+const displayElement = document.getElementById('numero-maggiore');
+console.log(numberOne);
+console.log(numberTwo);
+
+let message = 'il numero maggiore é ';
+
+if (numberOne > numberTwo){
+    console.log(numberOne);
+    message += numberOne;
+} else if (numberOne < numberTwo){
+    console.log(numberTwo);
+    message += nomberTwo;
+} else{
+    message = 'i numeri sono uguali'
+}
+
