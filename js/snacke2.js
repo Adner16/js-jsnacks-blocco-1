52a0ffc6ed8dd9fdf1e6e532d0aaedd4c7b21687
Adner16 <giovanni.renda16@gmail.com>
2022-01-12 15:13:19 +0100

    jsnacke2

--- js/snacke2.js
+++ js/snacke2.js
@@ -1,1 +1,12 @@
 console.log('js ok');
+
+wordOne = prompt('inserisci una parola','ciao');
+wordTwo = prompt('inserisci una seconda parola','ciaooooooo');
+
+if( wordOne < wordTwo){
+    console.log(wordOne);
+} else if (wordOne > wordTwo){
+    console.log (wordTwo);
+} else {
+    console.log('le parole hanno la stessa lunghezza');
+}
